f3a0ae5d8e3df68e4c42f49528b5737d7a4a0c8c
FlameWolf <agnijith@gmail.com>
2022-08-05 21:54:26 +0530

    Update Startup.cs

--- AspDotNetCoreLesson/Startup.cs
+++ AspDotNetCoreLesson/Startup.cs
@@ -72,7 +72,6 @@ namespace AspDotNetCoreLesson
 			}).AddSwaggerExamplesFromAssemblies(Assembly.GetExecutingAssembly());
 		}
 
-		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
 		public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
 		{
 			if (env.IsDevelopment())
